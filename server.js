5a0890977a8ff86681da7248ad277b33aee2cf8b
ceokornten <ceo@kornten.com>
2020-02-04 21:13:27 +0700

    SRB Landing Page

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/land",function(req, res){
+   
+    res.sendFile(__dirname + "/land001.html");
+});
+
+app.listen(3000, function(){
+    console.log("server start on port 3000");
+});
+
